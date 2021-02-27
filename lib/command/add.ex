af182c42590d7eeaa95cce98f3bf727c064b24cf
EdmondFrank <Edmondfrank@hotmail.com>
2021-02-27 23:36:57 +0800

    refactor(add): refactor add command

--- lib/command/add.ex
+++ lib/command/add.ex
@@ -14,19 +14,10 @@ defmodule Egit.Command.Add do
     {:ok, index} = Index.load_for_update(repo.index)
 
     try do
-      index  = Enum.slice(args, 1..-1)
-      |> Enum.reduce(index, fn path, index ->
-        Enum.reduce(Workspace.list_files(repo.workspace, path), index, fn sub_path, sub_index ->
-          data = Workspace.read_file(repo.workspace, sub_path)
-          stat = Workspace.stat_file(repo.workspace, sub_path)
+      Enum.slice(args, 1..-1)
+      |> Enum.reduce(index, &add_to_index(repo, &1, &2))
+      |> Index.write_updates
 
-          blob = Blob.new(data)
-          blob = Database.store(repo.database, blob)
-          Index.add(sub_index, sub_path, blob, stat)
-        end)
-      end)
-
-      Index.write_updates(index)
     rescue
       e in Error.MissingFile ->
         IO.puts(:stderr, "fatal: #{e.message}")
@@ -39,5 +30,23 @@ defmodule Egit.Command.Add do
     end
 
     exit(:normal)
+  end
+
+  defp expanded_paths(repo, path) do
+    Workspace.list_files(repo.workspace, path)
+  end
+
+  defp add_to_index(repo, path, index) do
+    Enum.reduce(
+      expanded_paths(repo, path),
+      index,
+      fn sub_path, sub_index ->
+        data = Workspace.read_file(repo.workspace, sub_path)
+        stat = Workspace.stat_file(repo.workspace, sub_path)
+
+        blob = Blob.new(data)
+        blob = Database.store(repo.database, blob)
+        Index.add(sub_index, sub_path, blob, stat)
+    end)
   end
 end
